812e7e198e919482b04a40e87e31c3adb23bbbca
alex2534 <alex2534@outlook.com>
2024-07-05 12:56:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Ecola Dev</h1>
+      <br />
+      <hr />
+    </>
+  );
+}
+
+export default App;
